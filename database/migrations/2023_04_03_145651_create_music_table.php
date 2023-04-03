656512b8b88e09dcd05a88d4ceca29f17611cdb0
Aribunfi <arianna@pabis.de>
2023-04-03 17:02:21 +0200

    voci della tabella musicale

---
+++ database/migrations/2023_04_03_145651_create_music_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('music', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('album');
+            $table->string('author');
+            $table->string('editor');
+            $table->string('length');
+            $table->string('poster');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('music');
+    }
+};
