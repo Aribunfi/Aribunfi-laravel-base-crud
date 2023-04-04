59787ece073a2d6d21c5066034a0709aa81302b6
Aribunfi <arianna@pabis.de>
2023-04-04 15:35:25 +0200

    spostamento navbar nel suo partial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,40 +13,7 @@
 
 <body>
 
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Bootstrap Music</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Album</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Artisti
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Italiani</a></li>
-              <li><a class="dropdown-item" href="#">Stranieri</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Collaborazioni</a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Nome brano o album" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Cerca</button>
-        </form>
-      </div>
-    </div>
-  </nav>
-
+  @include('partials._navbar')
 
 
 
