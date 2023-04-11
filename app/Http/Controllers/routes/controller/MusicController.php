360e16c7a6e6620a780dc6701739dd9188ce3a0e
Aribunfi <arianna@pabis.de>
2023-04-11 08:28:57 +0200

    validator

--- app/Http/Controllers/routes/controller/MusicController.php
+++ app/Http/Controllers/routes/controller/MusicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\routes\controller;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Music;
 
 use Illuminate\Http\Request;
@@ -57,5 +58,45 @@ public function destroy(Music $music)
     $music->delete();
     return redirect()->route('music.index');
 }
+
+
+private function validation($data) {
+    $validator = Validator::make(
+      $data,
+      [
+        'title' => 'required|string|max:20',
+        'album' => 'required|string|max:20',
+        "author" => 'required|string|max:20',
+        "editor" => 'required|string|max:20',
+        "length" => "required|integer",
+        "poster" => "nullable|string",
+      ],
+      [
+        'title.required' => 'Il titolo è obbligatorio',
+        'title.string' => 'Il titolo deve essere una stringa',
+        'title.max' => 'Il nome deve massimo di 20 caratteri',
+  
+        'album.required' => 'Album è obbligatorio',
+        'album.string' => 'Album deve essere una stringa',
+        'album.max' => 'Album deve massimo di 20 caratteri',
+          
+        'author.required' => 'Autore è obbligatorio',
+        'author.string' => 'Autore deve essere una stringa',
+        'author.max' => 'Autore deve massimo di 20 caratteri',
+        
+        'editor.required' => 'Editor è obbligatorio',
+        'editor.string' => 'Editor deve essere una stringa',
+        'editor.max' => 'Editor deve massimo di 20 caratteri',
+        
+        'length.required' => 'La lunghezza è obbligatoria',
+        'length.integer' => 'La lunghezza deve essere un numero',
+        
+        'poster.string' => 'L\'immagine deve essere una stringa',
+        
+      ]
+    )->validate();
+  
+    return $validator;
+  }
     
 }
