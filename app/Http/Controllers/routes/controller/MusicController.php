d65c598d2c6235a32cd693fb6b709cfd2da3cc5f
Aribunfi <arianna@pabis.de>
2023-04-11 07:19:03 +0200

    importazione modello nel controller

--- app/Http/Controllers/routes/controller/MusicController.php
+++ app/Http/Controllers/routes/controller/MusicController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\routes\controller;
 
 use App\Http\Controllers\Controller;
+use App\Models\Music;
+
 use Illuminate\Http\Request;
 
 class MusicController extends Controller
