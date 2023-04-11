f6c8e3c64f793d962c5f2661a9f85839b0300515
Aribunfi <arianna@pabis.de>
2023-04-11 08:17:51 +0200

    gestione eliminazione record

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -38,3 +38,32 @@
 {{ $music->links('pagination::bootstrap-5') }}
 
 
+@foreach ($music as $music)
+  <!-- Modal -->
+  <div class="modal fade" id="delete-modal-{{ $pasta->id }}" tabindex="-1" aria-labelledby="delete-modal-{{ $music->id }}-label"
+    aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="delete-modal-{{ $music->id }}-label">Conferma eliminazione</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body text-start">
+          Sei sicuro di voler eliminare il brano {{ $music->title }} Author {{ $music->author }} con ID
+          {{ $music->id }}? <br>
+          L'operazione non è reversibile
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+
+          <form action="{{ route('music.destroy', $music) }}" method="POST" class="">
+            @method('DELETE')
+            @csrf
+
+            <button type="submit" class="btn btn-danger">Elimina</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+@endforeach
